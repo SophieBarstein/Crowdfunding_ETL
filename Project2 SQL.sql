ab4c335890b746b396f7708902802dde48576178
martin0s <martiraf25@gmail.com>
2023-09-21 21:41:56 -0400

    crowdfunding

---
+++ Project2 SQL.sql
@@ -0,0 +1,60 @@
+﻿-- drop tables if exist
+
+drop table if exists contacts;
+drop table if exists category;
+drop table if exists subcategory;
+drop table if exists campaign;
+
+-- create contacts table and set our primary key
+
+CREATE TABLE contacts (
+    contact_id int NOT NULL,
+    first_name varchar (150)  NOT NULL,
+    last_name varchar (150)  NOT NULL,
+    email varchar (150)  NOT NULL,
+	primary key (contact_id)
+);
+
+select * from contacts
+
+
+CREATE TABLE campaign (
+    cf_id INT NOT NULL,
+    contact_id INT NOT NULL,
+	company_name VARCHAR(50) NOT NULL,
+    description VARCHAR (150)  NOT NULL,
+	goal INT NOT NULL,
+	pledged INT NOT NULL,
+	outcome VARCHAR (20) NOT NULL,
+	backers_count INT NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	currency VARCHAR (50) NOT NULL,
+	launched_date DATE,
+	end_date DATE,
+	category_id VARCHAR (50) NOT NULL,
+	subcategory_id VARCHAR (50) NOT NULL,
+	primary key (cf_id)
+);
+
+select * from campaign
+
+
+
+CREATE TABLE subcategory (
+    subcategory_id varchar(50) NOT NULL,
+    subcategory varchar (50)  NOT NULL,
+	primary key (subcategory_id)
+);
+
+select * from subcategory
+
+
+
+CREATE TABLE category (
+    category_id varchar(50) NOT NULL,
+    category varchar (50)  NOT NULL,
+	primary key (category_id)
+);
+
+select * from category
+
